6f161fa8f0f6a2f3fa2ec9357bd85e120ac80dcc
mheiskan <mheiskan>
2002-10-22 05:54:20 +0000

    Lisäsin meteorologisten tuulinuolten piirtämisen, jonka saa päälle käskyllä "arrowpath meteorological". Samalla korjasin labels <dx> <dy> toimintaa, lasketut hilakoordinaatit pyöristettiin virheellisesti aina alaspäin static_cast<int> käskyllä, nyt käytössä on FmiRound.

--- qdcontour.cpp
+++ qdcontour.cpp
@@ -187,6 +187,100 @@ NFmiColorTools::Color ToColor(const string & theColor)
 }
 
 // ----------------------------------------------------------------------
+/*!
+ * This function returns the meteorological arrow for the given wind
+ * speed as a NFmiPath. The arrow is suitable for stroking but not
+ * for filling.
+ *
+ * If the speed is negative or kFloatMissing, an empty path is returned.
+ *
+ * Note the following details
+ * \code
+ *  1.25- 3.75 = 1 short on the side, plus on upward extra segment
+ *  3.75- 6.25 = 1 long
+ *  6.25- 8.75 = 1 long, 1 short
+ *  8.75-11.25 = 2 long
+ * 11.25-13.75 = 2 long, 1 short
+ * \endcode
+ *
+ * \param theSpeed The wind speed.
+ * \return The path of the meteorological arrow.
+ * \todo Speeds over 50 are not supported yet
+ */
+// ----------------------------------------------------------------------
+
+NFmiPath MetArrow(float theSpeed)
+{
+  NFmiPath path;
+
+  // Handle bad cases
+  if(theSpeed<0 || theSpeed==kFloatMissing)
+	return path;
+
+  // The details of the flag
+
+  const float spot_size = 0.75;			// size of the spot at the origin
+  const float initial_length = 14;		// length of the initial line for speed 0
+  const float flag_interval = 4;		// the spacing between the speed indicators
+  const float flag_length = 10;			// the length of a full speed indicator
+
+  const float flag_angle_deg = 60;		// angle from the direction the wind is coming from
+
+  const float flag_angle = flag_angle_deg/180*3.14159265358979323846;
+
+  // Mark the spot with a small dot
+  path.MoveTo(spot_size,spot_size);
+  path.LineTo(spot_size,-spot_size);
+  path.LineTo(-spot_size,-spot_size);
+  path.LineTo(-spot_size,spot_size);
+  path.LineTo(spot_size,spot_size);
+
+  // Start rendering
+
+  const float full_unit = 5;
+  const float half_unit = full_unit/2;
+  const float quarter_unit = full_unit/4;	// limit between rounding up and down
+
+  const unsigned int long_segments = static_cast<unsigned int>(::floor((theSpeed+quarter_unit)/full_unit));
+  const unsigned int short_segments = static_cast<unsigned int>(::floor((theSpeed-long_segments*full_unit+quarter_unit)/half_unit));
+
+  const bool has_extra_up = (theSpeed >= quarter_unit && theSpeed < full_unit - quarter_unit);
+
+  // The long streak upwards
+
+  path.MoveTo(0,spot_size);
+  path.LineTo(0,spot_size
+			  + initial_length
+			  + (std::max(1u,long_segments+short_segments)-1)*flag_interval
+			  + (has_extra_up ? flag_interval : 0));
+
+  // First the short segments (should be only one, according to present settings)
+
+  float y = spot_size + initial_length;
+
+  if(short_segments>0)
+	for(unsigned int i=0; i<short_segments; i++)
+	  {
+		path.MoveTo(0,y);
+		path.LineTo(flag_length/2*::sin(flag_angle),y+flag_length/2*::cos(flag_angle));
+		y += flag_interval;
+	  }
+  
+  // Then the long ones
+  
+  if(long_segments>0)
+	for(unsigned int j=0; j<long_segments; j++)
+	  {
+		path.MoveTo(0,y);
+		path.LineTo(flag_length*::sin(flag_angle),y+flag_length*::cos(flag_angle));
+		y += flag_interval;
+	  }
+  
+  return path;
+
+}
+
+// ----------------------------------------------------------------------
 // Yksittäisen contour-arvon säilytysluokka
 // ----------------------------------------------------------------------
 
@@ -2367,10 +2461,12 @@ int main(int argc, const char *argv[])
 								  
 								  if(fabs(ij.X()-FmiRound(ij.X()))<0.00001 &&
 									 fabs(ij.Y()-FmiRound(ij.Y()))<0.00001)
-									value = vals[static_cast<int>(ij.X())][static_cast<int>(ij.Y())];
+									{
+									  value = vals[FmiRound(ij.X())][FmiRound(ij.Y())];
+									}
 								  else
 									{
-									  int i = static_cast<int>(ij.X());
+									  int i = static_cast<int>(ij.X()); // rounds down
 									  int j = static_cast<int>(ij.Y());
 									  float v00 = vals.At(i,j,kFloatMissing);
 									  float v10 = vals.At(i+1,j,kFloatMissing);
@@ -2382,6 +2478,7 @@ int main(int argc, const char *argv[])
 																			  v00,v10,
 																			  v01,v11))
 										value = kFloatMissing;
+
 									}
 								  piter->AddLabelValue(value);
 								}
@@ -2590,22 +2687,24 @@ int main(int argc, const char *argv[])
 						{
 						  // Read the arrow definition
 						  
-						  ifstream arrow(theArrowFile.c_str());
-						  if(!arrow)
-							{
-							  cerr << "Error: Could not open " << theArrowFile << endl;
-							  exit(1);
-							}
-						  // Read in the entire file
-						  
-						  string pathstring;
-						  StringReader(arrow,pathstring);
-						  arrow.close();
-						  
-						  // Convert to a path
-						  
 						  NFmiPath arrowpath;
-						  arrowpath.Add(pathstring);
+						  if(theArrowFile != "meteorological")
+							{
+							  ifstream arrow(theArrowFile.c_str());
+							  if(!arrow)
+								{
+								  cerr << "Error: Could not open " << theArrowFile << endl;
+								  exit(1);
+								}
+							  // Read in the entire file
+							  string pathstring;
+							  StringReader(arrow,pathstring);
+							  arrow.close();
+
+							  // Convert to a path
+							  
+							  arrowpath.Add(pathstring);
+							}
 						  
 						  // Handle all given coordinates
 						  
@@ -2647,7 +2746,12 @@ int main(int argc, const char *argv[])
 							  // Create a new path
 							  
 							  NFmiPath thispath;
-							  thispath.Add(arrowpath);
+
+							  if(theArrowFile == "meteorological")
+								thispath.Add(MetArrow(speed*theWindArrowScaleC));
+							  else
+								thispath.Add(arrowpath);
+
 							  if(speed>0 && speed!=kFloatMissing)
 								thispath.Scale(theWindArrowScaleA*log10(theWindArrowScaleB*speed+1)+theWindArrowScaleC);
 							  thispath.Scale(theArrowScale);
@@ -2688,7 +2792,7 @@ int main(int argc, const char *argv[])
 									// The start point
 									
 									NFmiPoint xy0 = theArea.ToXY(latlons[i][j]);
-									
+
 									// Direction calculations
 									
 									const float pi = 3.141592658979323;
@@ -2710,7 +2814,10 @@ int main(int argc, const char *argv[])
 									// Create a new path
 									
 									NFmiPath thispath;
-									thispath.Add(arrowpath);
+									if(theArrowFile == "meteorological")
+									  thispath.Add(MetArrow(speed*theWindArrowScaleC));
+									else
+									  thispath.Add(arrowpath);
 									if(speed>0 && speed != kFloatMissing)
 									  thispath.Scale(theWindArrowScaleA*log10(theWindArrowScaleB*speed+1)+theWindArrowScaleC);
 									thispath.Scale(theArrowScale);
@@ -2798,8 +2905,8 @@ int main(int argc, const char *argv[])
 									  theImage.Composite(marker,
 														 markerrule,
 														 kFmiAlignCenter,
-														 static_cast<int>(xy.X()),
-														 static_cast<int>(xy.Y()),
+														 FmiRound(xy.X()),
+														 FmiRound(xy.Y()),
 														 markeralpha);
 									}
 								}
@@ -2813,8 +2920,8 @@ int main(int argc, const char *argv[])
 									  theImage.Composite(marker,
 														 markerrule,
 														 kFmiAlignCenter,
-														 static_cast<int>(pts[qi][i][j].X()),
-														 static_cast<int>(pts[qi][i][j].Y()),
+														 FmiRound(pts[qi][i][j].X()),
+														 FmiRound(pts[qi][i][j].Y()),
 														 markeralpha);
 								}
 							}
@@ -2904,7 +3011,7 @@ int main(int argc, const char *argv[])
 								  x = iter->second.X();
 								  y = iter->second.Y();
 								}
-							  
+
 							  // Set new text properties
 							  
 							  text.Text(strvalue);
