2b72fac7a59bd55f8e0f2e30fa406ade1af73c1b
mheiskan <mheiskan>
2003-02-11 13:25:02 +0000

    Lisäsin ContourCache luokan, joka tallettaa annettuja kontuureja. Kontuurit tunnistetaan kontuurin raja-arvojen ja querydatan asetusten perusteella. Tunnistus ei ole vuorenvarmaa, joten asetuksilla ei saa temppuilla liikaa cachea käytettäessä.

---
+++ include/ContourCache.h
@@ -0,0 +1,89 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Interface of class ContourCache
+ */
+// ======================================================================
+/*!
+ * \class ContourCache
+ * \brief Storage for calculated contours
+ *
+ * The purpose of the ContourCache is to remember calculated contours
+ * and serve them back on demend.
+ *
+ * Each saved contour is identified by a set of parameters, such
+ * as the parameter name, the lower and upper limit of the contour,
+ * the time of the data and so on. The set of parameters is by no
+ * means completely unique, so one should not change settings too
+ * much between rendering on different maps, otherwise wrong results
+ * may be drawn.
+ *
+ * A good principle is to change nothing but the projection, the
+ * background and foreground images and the savepath.
+ *
+ * Typical use is shown below.
+ * \code
+ * ContourCache cache;
+ *
+ * NFmiPath path;
+ * if(cache.contains(lolimit, hilimit, querydata))
+ *    path = cache.find(lolimit, hilimit, querydata);
+ * else
+ * {
+ *    path = ... some means of calculating it;
+ *    cache.insert(path, lolimit, hilimit, querydata);
+ * }
+ * path.Project(area);
+ * path.Fill(image, color, rule);
+ * \endcode
+ */
+// ======================================================================
+
+#ifndef CONTOURCACHE_H
+#define CONTOURCACHE_H
+
+#include <map>
+#include <string>
+class LazyQueryData;
+class NFmiPath;
+
+class ContourCache
+{
+private:
+
+  typedef std::map<std::string, NFmiPath> storage_type;
+  storage_type itsData;
+
+public:
+
+  typedef storage_type::size_type size_type;
+
+#ifdef NO_COMPILER_GENERATED
+  ~ContourCache();
+  ContourCache();
+  ContourCache(const ContourCache & theCache);
+  ContourCache & operator=(const ContourCache & theCache);
+#endif
+
+  bool empty() const;
+  void clear();
+  size_type size() const;
+
+  bool contains(float theLoLimit,
+				float theHiLimit,
+				const LazyQueryData & theData) const;
+
+  const NFmiPath & find(float theLoLimit,
+						float theHiLimit,
+						const LazyQueryData & theData) const;
+
+  void insert(const NFmiPath & thePath,
+			  float theLoLimit,
+			  float theHiLimit,
+			  const LazyQueryData & theData);
+
+}; // class ContourCache
+
+#endif // CONTOURCACHE_H
+
+// ======================================================================
---
+++ source/ContourCache.cpp
@@ -0,0 +1,156 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Implementation of class ContourCache
+ */
+// ======================================================================
+
+#include "ContourCache.h"
+#include "LazyQueryData.h"
+#include "NFmiPath.h"
+#include "NFmiMetTime.h"
+#include <sstream>
+
+using namespace std;
+
+namespace
+{
+  // ----------------------------------------------------------------------
+  /*!
+   * \brief Return a cache-key for the given contour settings
+   *
+   * \param theLoLimit The lower limit of the contour
+   * \param theHiLimit The upper limit of the contour
+   * \param theData The query data
+   * \return The key for the data in the cache
+   */
+  // ----------------------------------------------------------------------
+
+  std::string cache_key(float theLoLimit,
+						float theHiLimit,
+						const LazyQueryData & theData)
+  {
+	ostringstream os;
+
+	os << theLoLimit << '_'
+	   << theHiLimit << '_'
+	   << theData.Filename() << '_'
+	   << theData.ValidTime().ToStr(kYYYYMMDDHHMM).CharPtr() << '_'
+	   << theData.OriginTime().ToStr(kYYYYMMDDHHMM).CharPtr() << '_'
+	   << theData.GetParamName() << '_'
+	   << theData.GetParamIdent();
+
+	return os.str();
+
+  }
+}
+
+// ----------------------------------------------------------------------
+/*!
+ * \brief Test if the cache is empty
+ *
+ * \return True if the cache is empty
+ */
+// ----------------------------------------------------------------------
+
+bool ContourCache::empty() const
+{
+  return itsData.empty();
+}
+
+// ----------------------------------------------------------------------
+/*!
+ * \brief Empty the cache
+ */
+// ----------------------------------------------------------------------
+
+void ContourCache::clear()
+{
+  itsData.clear();
+}
+
+// ----------------------------------------------------------------------
+/*!
+ * \brief Return the number of cached contours
+ *
+ * \return The number of cached contours
+ */
+// ----------------------------------------------------------------------
+
+ContourCache::size_type ContourCache::size() const
+{
+  return itsData.size();
+}
+
+// ----------------------------------------------------------------------
+/*!
+ * \brief Test if given contour is cached
+ *
+ * \param theLoLimit The lower limit of the contour
+ * \param theHiLimit The upper limit of the contour
+ * \param theData The query data
+ */
+// ----------------------------------------------------------------------
+
+bool ContourCache::contains(float theLoLimit,
+							float theHiLimit,
+							const LazyQueryData & theData) const
+{
+  string key = cache_key(theLoLimit, theHiLimit, theData);
+  storage_type::const_iterator it = itsData.find(key);
+  return (it != itsData.end());
+}
+
+// ----------------------------------------------------------------------
+/*!
+ * \brief Find the given cached contour
+ *
+ * This will throw if the contour is not cached
+ *
+ * \param theLoLimit The lower limit of the contour
+ * \param theHiLimit The upper limit of the contour
+ * \param theData The query data
+ * \return The path
+ */
+// ----------------------------------------------------------------------
+
+const NFmiPath & ContourCache::find(float theLoLimit,
+									float theHiLimit,
+									const LazyQueryData & theData) const
+{
+  string key = cache_key(theLoLimit, theHiLimit, theData);
+  storage_type::const_iterator it = itsData.find(key);
+  if(it != itsData.end())
+	return it->second;
+  throw runtime_error("Contour was not in the cache - use contains first!");
+}
+
+// ----------------------------------------------------------------------
+/*!
+ * \brief Insert a new path into the cache
+ *
+ * This will throw if the contour is already cached
+ *
+ * \param thePath The path to cache
+ * \param theLoLimit The lower limit of the contour
+ * \param theHiLimit The upper limit of the contour
+ * \param theData The query data
+ */
+// ----------------------------------------------------------------------
+
+void ContourCache::insert(const NFmiPath & thePath,
+						  float theLoLimit,
+						  float theHiLimit,
+						  const LazyQueryData & theData)
+{
+  string key = cache_key(theLoLimit, theHiLimit, theData);
+
+  typedef pair<storage_type::const_iterator, bool> restype;
+
+  restype result = itsData.insert(storage_type::value_type(key,thePath));
+  
+  if(!result.second)
+	throw runtime_error("Contour was already in the cache!");
+}
+
+// ======================================================================
