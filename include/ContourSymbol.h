515d5aa75978dbce091e9dc194a7d076d15071ba
mheiskan <mheiskan>
2004-01-12 13:48:38 +0000

    *** empty log message ***

---
+++ include/ContourSymbol.h
@@ -0,0 +1,44 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief Interface of class ContourSymbol
+ */
+// ======================================================================
+/*!
+ * \class ContourSymbol
+ * \brief Storage for a single contour symbol
+ *
+ */
+// ======================================================================
+
+#ifndef CONTOURSYMBOL_H
+#define CONTOURSYMBOL_H
+
+#include "ContourPattern.h"
+
+// The interface is exactly the same, no sense in wasting code
+// However, a typedef is not enough to get proper operator overloading,
+// instead we use inheritance to get a new type
+
+class ContourSymbol : public ContourPattern
+{
+public:
+  ContourSymbol(float theLoLimit,
+				float theHiLimit,
+				const std::string & thePattern,
+				const std::string & theRule,
+				float theFactor=1.0)
+	: ContourPattern(theLoLimit,
+					 theHiLimit,
+					 thePattern,
+					 theRule,
+					 theFactor)
+  {
+  }
+
+};
+
+#endif // CONTOURSYMBOL_H
+
+// ======================================================================
+
